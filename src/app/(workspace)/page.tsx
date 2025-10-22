581b6047e7d28bad3bd943c82e06d139f5b6195c
Abhishek Gogoi <abhisat495@gmail.com>
2025-10-22 20:06:51 +0530

    04-Header

---
+++ src/app/(workspace)/page.tsx
@@ -0,0 +1,13 @@
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/authentication/components/user-button";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  const user = await currentUser();
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <UserButton user={user} />
+    </div>
+  );
+}
